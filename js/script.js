536914f7150afcb1b6d066469d5b3a0d36622147
pavelkovalevski <108361722+pavelkovalevski@users.noreply.github.com>
2022-11-03 17:16:01 +0300

    final version

--- js/script.js
+++ js/script.js
@@ -1,32 +1,25 @@
 const hamburger = document.querySelector('.hamburger'),
-menu = document.querySelector('.menu'),
+menu = document.querySelector('.header__menu'),
 menuLink = document.querySelectorAll('.menu__link'),
 form = document.querySelector('.form'),
-formSelectList = form.querySelector('.form-input__select-list'),
-formSelectItems = form.querySelectorAll('.form-input__select-list-item'),
-formSelectBtn = form.querySelector('.form-input__select-button'),
-formSelectArrow = form.querySelector('.form-input__select-arrow'),
-formSelectInput = form.querySelector('.form-input__select-input'),
-formPecentInput = form.querySelector('.form-input__percent input'),
-formPecentValue = form.querySelector('.form-input__percent-value'),
-formFile = form.querySelector('.form-input__file'),
-formFileLabel = formFile.querySelector('label'),
-formFileInput = formFile.querySelector('input'),
-formEmail = form.querySelector('.form-input__email'),
-formEmailInput = formEmail.querySelector('input'),
-formEmailLabel = formEmail.querySelector('label');
-/* formName = form.querySelector('.form-input__name'),
-formNameInput = formName.querySelector('input'),
-formNameLabel = formName.querySelector('label'); */
+formSelectList = form.querySelector('.input-select__list'),
+formSelectItems = form.querySelectorAll('.input-select__item'),
+formSelectBtn = form.querySelector('.input-select__button'),
+formSelectArrow = form.querySelector('.input-select__arrow'),
+formSelectInput = form.querySelector('.input-select__input'),
+formPecentInput = form.querySelector('.input-percent__input'),
+formPecentValue = form.querySelector('.input-percent__value'),
+formFileInput = form.querySelector('.input-file__input');
+formFileLabel = form.querySelector('.input-file__label'),
 
 //menu
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('menu_active');
+    menu.classList.toggle('header__menu_active');
 });
 
 menuLink.forEach(item => {
     item.addEventListener('click', () => {
-        menu.classList.remove('menu_active');
+        menu.classList.remove('header__menu_active');
     });
 });
 
@@ -38,25 +31,20 @@ function toggleClass(element, elementClass) {
 formSelectBtn.addEventListener('click', function(e) {
     e.preventDefault();
 
-    toggleClass(formSelectBtn, 'form-input__select-button_active');
-    toggleClass(formSelectArrow, 'form-input__select-arrow_active');
-    toggleClass(formSelectList, 'form-input__select-list_active');
+    toggleClass(formSelectBtn, 'input-select__button_active');
+    toggleClass(formSelectArrow, 'input-select__arrow_active');
+    toggleClass(formSelectList, 'input-select__list_active');
 });
 
 formSelectItems.forEach(function(item) {
     item.addEventListener('click', function() {
         formSelectBtn.innerHTML = this.innerHTML;
-        toggleClass(formSelectBtn, 'form-input__select-button_active');
-        toggleClass(formSelectArrow, 'form-input__select-arrow_active');
-        toggleClass(formSelectList, 'form-input__select-list_active');
+        toggleClass(formSelectBtn, 'input-select__button_active');
+        toggleClass(formSelectArrow, 'input-select__arrow_active');
+        toggleClass(formSelectList, 'input-select__list_active');
         formSelectInput.value = this.dataset.value;
     });
 });
-
-//inputs email and name
-/* formEmailInput.addEventListener('click', function() {
-    formEmailLabel.style.display = "none";
-}); */
 
 //slider
 formPecentInput.addEventListener('input', function() {
